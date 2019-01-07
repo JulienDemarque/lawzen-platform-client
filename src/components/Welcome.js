15b1b4b7d9eddfb158cec7b2072440ecf6ed9239
julien demarque <julien_demarque@hotmail.fr>
2019-01-07 19:40:20 +0200

    added voting on law

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { connect } from "react-redux";
 import "bulma/css/bulma.css";
 //I need to change the render according to if we are login or not. see firebase-react-login about that
 
@@ -16,6 +17,25 @@ class Welcome extends React.Component {
   async getRecentLaws() {
     const response = await axios.get("http://localhost:3090/recent");
     this.setState({ recent: response.data });
+    console.log(response);
+  }
+
+  handleClick = e => {
+    this.sendNewUpvote(e.currentTarget.name);
+  };
+
+  async sendNewUpvote(lawTitle) {
+    const token = localStorage.getItem("token");
+    const config = { headers: { authorization: token } };
+
+    const response = await axios.post(
+      `http://localhost:3090/upvote/${lawTitle}`,
+      { lawTitle },
+      config
+    );
+    console.log(response);
+    //update the state with the new vote or unvote
+    this.getRecentLaws();
   }
 
   render() {
@@ -29,8 +49,29 @@ class Welcome extends React.Component {
           {this.state.recent.map(law => {
             return (
               <div key={law.title} className="notification">
-                <h6 className="title is-6">{law.title}</h6>
-                <p>{law.description}</p>
+                <div className="columns">
+                  <div className="column">
+                    <h6 className="title is-6">{law.title}</h6>
+                    <p>{law.description}</p>
+                  </div>
+                  <div className="column">
+                    <div className="has-text-right m-b-xl">
+                      <button
+                        onClick={this.handleClick}
+                        name={law.title}
+                        className="button is-large is-dark"
+                      >
+                        <span className="icon is-big is-left">
+                          <i className="fas fa-vote-yea" />
+                        </span>
+                      </button>
+                      <p>Upvote: {law.upVotes.length}</p>
+                    </div>
+                    <p className="has-text-right">
+                      <strong> - {law.author.username}</strong>
+                    </p>
+                  </div>
+                </div>
               </div>
             );
           })}
@@ -40,4 +81,11 @@ class Welcome extends React.Component {
   }
 }
 
-export default Welcome;
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated,
+    username: state.auth.username
+  };
+}
+
+export default connect(mapStateToProps)(Welcome);
